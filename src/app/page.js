91aa805c9cae7e075202f1cdaef252f08d996d57
RangelTomazilli <rangel.tomazilli@gmail.com>
2023-11-20 22:08:13 -0300

    fix: icones botoes.

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ const getData = async () => {
         url
       }
     }
-  }
+  }  
   `
   return fetchHygraphQuery(query)
 }
@@ -37,12 +37,12 @@ export default async function Home() {
         height={400}
       />
       <div>
-        <div className='font-semibold text-gray-700 text-center'> <RichText content={response.home.introduction.raw} /> </div>
+        <div className='font-semibold text-gray-700 text-center'> <RichText content={response.home.introduction.raw} /></div>
       </div>
       <div className="flex flex-col items-center justify-center gap-5 w-full lg:w-1/2">
         {
           response.home.links.map(content => (
-            <ButtonLinkWithIcon key={content.name} text={content.name} icon={content.iconSvg} link={content.url} iconColorClass={content.iconColor} />
+            <ButtonLinkWithIcon key={content.name} text={content.name} icon={content.iconSvg} link={content.url} />
           ))
         }
       </div>
--- src/components/ButtonLinkWithIcon.js
+++ src/components/ButtonLinkWithIcon.js
@@ -1,14 +1,14 @@
 "use client";
 
-export default function ButtonLinkWithIcon({ text, icon, link, iconColorClass }) {
+export default function ButtonLinkWithIcon({ text, icon, link }) {
     const handle = () => {
         const handleLink = link;
         window.open(handleLink, "_blank");
     };
     return (
-        <button type="button" class="flex w-full justify-center rounded-md border border-gray-300 px-3 py-1.5 font-semibold leading-6  shadow-sm hover:bg-gray-300" onClick={handle}>
+        <button type="button" className="flex w-full justify-center rounded-md border border-gray-300 px-3 py-1.5 font-semibold leading-6  shadow-sm hover:bg-gray-300" onClick={handle}>
             <div className='flex items-center gap-1'>
-                <div className={`text-2xl ${iconColorClass}`} dangerouslySetInnerHTML={{ __html: icon }} />
+                <div className={`text-sm`} dangerouslySetInnerHTML={{ __html: icon }} />
                 <div className="text-gray-500 hover:text-white text-sm">{text}</div>
             </div>
         </button>
